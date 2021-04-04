c7d6085a209947f3d8f9d2a7c1700df01209af57
liphone <974122$07@qq.com>
2021-04-04 14:36:58 +0800

    add paper link commit in 2021-04-04 14:36:57 by 97412

--- update.sh
+++ update.sh
@@ -2,6 +2,6 @@
 git add .
 time_str=$(date "+%Y-%m-%d %H:%M:%S")
 git commit -m "${1} commit in ${time_str} by `whoami`"
-git push -f origin master
+git push origin master
 git push github master
 exit
